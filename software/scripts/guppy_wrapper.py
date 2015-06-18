99edcab3b6831be1714865aeb7fbebd79c7ff5b6
Anais-Barray <anais.barray@gmail.com>
2015-06-18 10:38:52 +0200

    script v6

---
+++ software/scripts/guppy_wrapper.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+import subprocess, os, sys
+
+if __name__ == "__main__":
+	
+	ijson = sys.argv[1]
+	output = sys.argv[2]
+	
+	#fichier temporaire avec l'extension correcte pour etre reconnu par pplacer
+	ijsontemp = ""+os.path.splitext(ijson)[0]+".json" 
+
+	#copie pour pas modifier le fichier d'origine
+	subprocess.call(["cp", ijson, ijsontemp]) 
+	
+	#unix : /home/anaisb/software/pplacer/pplacer/pplacer -t $inputtree -s $inputstat $inputcombo -o $output
+	subprocess.call(["/home/anaisb/software/pplacer/pplacer/guppy", "tog", "--xml", "-o", output, ijsontemp])
+
+	#supprime le fichier temporaire devenu obsolete
+	subprocess.call(["rm", ijsontemp]) 
