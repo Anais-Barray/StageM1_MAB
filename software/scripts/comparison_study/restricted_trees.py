a9c2878d3c84f8802e3ab814916d2892ea302ca8
Anais-Barray <anais.barray@gmail.com>
2015-07-10 18:17:01 +0200

    script for restriction of trees

---
+++ software/scripts/comparison_study/restricted_trees.py
@@ -0,0 +1,74 @@
+#!usr/bin/python3
+import subprocess, os, sys, string
+from Bio import SeqIO
+
+subprocess.call("clear")
+
+if __name__ == "__main__":
+
+	allseqfile = sys.argv[1]
+	refseqfile = sys.argv[2]
+	allseqtree = sys.argv[3]
+	placedseqtree = sys.argv[4]
+	outputname = sys.argv[5]
+	
+	#~ Listes des noms de taxons necessaires pour realiser les arbres 
+	#~ restreints avec nw_prune.
+	list_alltaxa = []
+	alltaxa = ""
+	list_refseqtaxa = []
+	refseqtaxa = ""
+	list_readtaxa = []
+	
+	for sequence in SeqIO.parse(open(allseqfile), "fasta"):
+		list_alltaxa.append(sequence.id)
+	alltaxa = ' '.join(list_alltaxa)
+	
+	for sequence in SeqIO.parse(open(refseqfile), "fasta"):
+		list_refseqtaxa.append(sequence.id)
+	refseqtaxa = ' '.join(list_refseqtaxa)
+	 
+	for taxon in list_alltaxa:
+		if not taxon in list_refseqtaxa:
+			list_readtaxa.append(taxon)
+	
+	#~ => Faire les arbres restreints avec nw_prune pour realiser :  
+	   #~ les arbres avec placements vrais (simules) "true" des reads + 
+	   #~ les arbres avec placement des reads "placed" par le logiciel 
+	   #~ (epa/pplacer/sepp).
+	#~ => Passe par un script bash car probleme avec nw_prune appele en 
+	   #~ subprocess (noms de taxons passent pas en string)
+	#~ => 10 versions du read placed pour une 
+	   #~ sequence true. Par la suite (TreeDist), on compare chaque 
+	   #~ arbre "placed" avec l'arbre "true", on doit faire autant 
+	   #~ d'arbre dans les deux cas car ils seront compares par paire 
+		  #~ (arbre_placed1 avec arbre_true1, arbre_placed2 avec 
+	   #~ arbre_true2, etc). Ils doivent donc etre dans le meme ordre 
+	   #~ d'apparition dans leur fichier respectif.
+	for read in list_readtaxa:
+		for i in range (0,10):
+			bashcmd_true = "/home/anaisb/software/newick-utils-1.6/src/nw_prune -v " + allseqtree + " " + refseqtaxa + " " + read + " >> " + outputname + "_output_restrictedTrees_true"
+			bashcmd_placed = "/home/anaisb/software/newick-utils-1.6/src/nw_prune -v " + placedseqtree + " " + refseqtaxa + " " + read + "_" + str(i) + " >> " + outputname + "_output_restrictedTrees_placed"
+			f = open("run_restrict.sh", "w")
+			f.write("#!/bin/bash\n")
+			f.write(bashcmd_true+"\n")
+			f.write(bashcmd_placed+"\n")
+			f.write("exit 0")
+			f.close()
+			subprocess.call(["chmod","755","run_restrict.sh"])
+			subprocess.call(["sh", "run_restrict.sh"])
+	
+	#~ #taxons des reads finissent par _#, l'enlever pour pouvoir comparer les arbres restreints.
+	f2 = open(outputname+"_output_restrictedTrees_placed","r")
+	f3 = open(outputname+"_output_restrictedTrees_placed_checked", "w")
+	patternlist = ["_0", "_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9"]
+	
+	for line in f2:
+		for pattern in patternlist:
+			line = line.replace(pattern,"")
+		f3.write(line)
+
+	f2.close()
+	f3.close()
+	
+	print "Restricted trees of <<<",outputname,">>> done.\n"
